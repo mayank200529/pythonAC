7dde28baf40afec808b185db90155ca4db32b2e7
mayank200529 <rawatmayank685@gmail.com>
2025-08-24 14:49:58 +0530

    The small project named Menu-driven.py is modified

--- SImple_Projects/Menu-driven.py
+++ SImple_Projects/Menu-driven.py
@@ -1,20 +1,29 @@
 #Menu-Driven Program
-print("Hello!\nThis program will help you to calculate area")
-print("To calculate area of the any shape please enter the no. corresponding to it")
-print("1.Circle\n2.Rectangle\n3.Square")
-num = int(input("Enter the corresponding number - "))
-if num == 1:
-    rad = int(input("Enter radius of circle(in cm) - "))
-    arofc = 3.14 * rad * rad
-    print(f"Area of circle is {arofc}cm sq")
-elif num == 2:
-    len = int(input("Enter length of rectangle(in cm) - "))
-    brd = int(input("Enter breadth of rectangle(in cm) - "))
-    arofr = len * brd
-    print(f"Area of rectangle is {arofr}cm sq")
-elif num == 3:
-    side = int(input("Enter side of square(in cm) - "))
-    arofs = side * side
-    print(f"Area of square is {arofs}cm sq")
-else:
-    print("Invalid input")
+def menu():  #declaring a function named menu()
+    print("Hello!\nThis program will help you to calculate area")
+    print("To calculate area of the any shape please enter the no. corresponding to it")
+    print("1.Circle\n2.Rectangle\n3.Square")
+    num = int(input("Enter the corresponding number - "))
+    if num == 1:
+        rad = int(input("Enter radius of circle(in cm) - "))
+        arofc = 3.14 * rad * rad
+        print(f"Area of circle is {arofc}cm sq")
+    elif num == 2:
+        len = int(input("Enter length of rectangle(in cm) - "))
+        brd = int(input("Enter breadth of rectangle(in cm) - "))
+        arofr = len * brd
+        print(f"Area of rectangle is {arofr}cm sq")
+    elif num == 3:
+        side = int(input("Enter side of square(in cm) - "))
+        arofs = side * side
+        print(f"Area of square is {arofs}cm sq")
+    else:
+        print("Invalid input")
+    
+    print("Do You Want To Continue?")
+    print("~Enter 1 to continue")
+    print("~Enter any other key to exit")
+    key = int(input("Enter here :- "))
+    if key == 1:
+        menu()
+menu()
