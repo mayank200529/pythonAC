1ed1c5f3b5aa82d19544c5002c35f145a7829d53
mayank200529 <rawatmayank685@gmail.com>
2025-08-28 22:25:50 +0530

    user enter small project  added in repo

---
+++ SImple_Projects/user_enter_0.py
@@ -0,0 +1,10 @@
+print("Enter any number to print")
+print("Enter 0 to stop")
+i = 1
+while i >= 1:
+    n = int(input("Enter no.- "))
+    if n == 0:
+        print("EXIT")
+        i = 0
+    else:
+        print(n)
