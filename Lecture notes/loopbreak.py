8aa1f1437bf71c67974cc0e297f2e058862d2758
mayank200529 <rawatmayank685@gmail.com>
2025-09-05 20:25:27 +0530

    added break continue pass file in repo

---
+++ Lecture notes/loopbreak.py
@@ -0,0 +1,15 @@
+# break continue pass
+
+for i in range(1,6):
+    pass
+
+
+for i in range(1,6):
+    for j in range(1,6):
+        print("X",end=" ")
+        if j == i:
+            break
+    print()
+
+
+    
